8d7599e8227c59f303c7cb9619faba3212d18aec
AlanVinicius357 <alanvinicius295@gmail.com>
2025-08-19 04:29:34 -0300

    curso de javascript

---
+++ aula011/ex008.js
@@ -0,0 +1,8 @@
+var vel = 59.5
+console.log(`a velocidade do seu carro é ${vel}Km/h`)
+
+if (vel > 60){ //isso é uma condição simples
+    console.log(`Você ultrapassou a velocidade permitida, MULTADO!`)
+}
+
+console.log(`Dirija sempre usando o cinto de segurança!`)
---
+++ aula011/ex009.js
@@ -0,0 +1,8 @@
+var pais = 'França'
+
+if (pais == 'Brasil'){ //condição composta
+    console.log(`Você é brasileiro!`)
+}
+else {
+    console.log(`Você é estrangeiro!`)
+}
